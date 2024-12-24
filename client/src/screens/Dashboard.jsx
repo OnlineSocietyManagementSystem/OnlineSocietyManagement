33a2351691da99d249cdfeef2dc8c86224d8a6b4
Dhananjay Gayke <dhanu915818@gmail.com>
2024-12-24 18:18:44 +0530

    added line to test from testBranch

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -11,6 +11,7 @@ function Dashboard() {
         <div className="flex-grow-1 p-4" style={{ marginLeft: "20%" }}>
             <h2>Dashboard</h2>
             <p>Here will be your announcements content.</p>
+            <p>Added line to test from testBranch</p>
        </div>
         
     </div>
