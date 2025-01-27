6f6979c8fecd1df97fe9b4bed502c1bc9d171ecb
pavankumar <pavankumar976747@gmail.com>
2025-01-27 12:05:40 +0530

    registration page development

--- client/src/screens/Register.jsx
+++ client/src/screens/Register.jsx
@@ -11,53 +11,92 @@ const Register = () => {
         style={{ width: "60%", height: "70%" }}
       >
         <h2 className="text-center mb-4 fw-bold">Registration Form</h2>
+
         <form>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              placeholder="Enter your name"
-            />
+          <div className="mb-4">
+            <div className="row">
+              <div className="col-md-6">
+                <label htmlFor="firstname" className="form-label">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="firstname"
+                  placeholder="Enter your first name"
+                />
+              </div>
+              <div className="col-md-6">
+                <label htmlFor="lastname" className="form-label">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastname"
+                  placeholder="Enter your last name"
+                />
+              </div>
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Enter your email"
-            />
+
+          <div className="mb-4">
+            <div className="row">
+              <div className="col-md-6">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Enter your email"
+                />
+              </div>
+
+              <div className="col-md-6">
+                <label htmlFor="role" className="form-label">
+                  Role
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="role"
+                  placeholder="Enter your role"
+                />
+              </div>
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter your password"
-            />
+
+          <div className="mb-4">
+            <div className="row">
+              <div className="col-md-6">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Enter your password"
+                />
+              </div>
+              <div className="col-md-6">
+                <label htmlFor="confirmpassword" className="form-label">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="confirmpassword"
+                  placeholder="Confirm your password"
+                />
+              </div>
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="phone" className="form-label">
-              Phone Number
-            </label>
-            <input
-              type="tel"
-              className="form-control"
-              id="phone"
-              placeholder="Enter your phone number"
-            />
-          </div>
-          <div>
-            <button type="submit" className="btn btn-primary w-30 text-center">
+
+          <div className="text-center">
+            <button type="submit" className="btn btn-primary w-40 ">
               Submit
             </button>
           </div>
