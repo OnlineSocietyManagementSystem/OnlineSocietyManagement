7a710efc9abc8f1055baee59cd38956591d8841a
D4-87101-SrushtiParlikar <parlikarsrushti@gmail.com>
2025-01-29 21:38:37 +0530

    payment successfull

--- server/src/main/java/com/society/controllers/PaymentController.java
+++ server/src/main/java/com/society/controllers/PaymentController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -14,7 +15,7 @@ import com.society.dtos.PaymentDto;
 import com.society.service.PaymentServiceImpl;
 
 import io.jsonwebtoken.io.IOException;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 public class PaymentController {
@@ -34,39 +35,10 @@ public class PaymentController {
          } catch (RuntimeException e) {
              return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage()));
          }
-     }
-	
-	
-	
-//	@PostMapping("/payment")
-//    public ResponseEntity<?> makePayment(@RequestBody String requestBody, Authentication authentication) {
-//        System.out.println("Raw JSON Received: " + requestBody);  // ✅ Print raw JSON before mapping
-//
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            PaymentDto paymentDto = objectMapper.readValue(requestBody, PaymentDto.class);  // Manually map JSON to DTO
-//
-//            System.out.println("Parsed DTO: " + paymentDto); // ✅ Print parsed object
-//
-//            String email = authentication.getName();
-//            ApiResponse response = paymentServiceImpl.makePayment(paymentDto, email);
-//            return ResponseEntity.status(HttpStatus.CREATED).body(response);
-//        } catch (IOException | JsonProcessingException e) {
-//            return ResponseEntity.badRequest().body("Invalid JSON format: " + e.getMessage());
-//        }
     }
+}
 	
-//	public ResponseEntity<?> makePayment(@RequestBody(required = false) PaymentDto paymentDto, Authentication authentication) {
-//	    if (paymentDto == null) {
-//	        return ResponseEntity.badRequest().body("Request body is missing or empty!");
-//	    }
-//
-//	    System.out.println("Parsed DTO: " + paymentDto); // Debugging
-//	    String email = authentication.getName();
-//
-//	    ApiResponse response = paymentServiceImpl.makePayment(paymentDto, email);
-//	    return ResponseEntity.status(HttpStatus.CREATED).body(response);
-//	}
-
+	
+	
 
 
