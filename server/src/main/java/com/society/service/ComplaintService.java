e52d04f0ddc80f0d347a7dd746f1cddd0f1bacd1
pavankumar <pavankumar976747@gmail.com>
2025-01-30 15:52:00 +0530

    changes in admincomplaintsfeedback

--- server/src/main/java/com/society/service/ComplaintService.java
+++ server/src/main/java/com/society/service/ComplaintService.java
@@ -1,10 +1,13 @@
 package com.society.service;
 
+import java.util.List;
+
 import com.society.dtos.ApiResponse;
 import com.society.dtos.ComplaintDto;
 
 public interface ComplaintService {
 	
 	ApiResponse addComplaint(ComplaintDto dto,String email);
+
 
 }
