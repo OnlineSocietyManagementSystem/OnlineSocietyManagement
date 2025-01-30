f502705ca324ec530b83a439477be75e5e24d243
pavankumar <pavankumar976747@gmail.com>
2025-01-30 15:10:21 +0530

    api added for fetching feedbacks and delete feedback

--- server/src/main/java/com/society/security/SecurityConfiguration.java
+++ server/src/main/java/com/society/security/SecurityConfiguration.java
@@ -34,9 +34,9 @@ public class SecurityConfiguration {
                 ).permitAll()
                 .requestMatchers(HttpMethod.OPTIONS).permitAll()
                 // Endpoints accessible only to members
-                .requestMatchers("/member/dashboard", "/member/details/**","/payment","/addComplaint", "/add-feedback")
+                .requestMatchers("/member/dashboard", "/member/details/**","/payment","/addComplaint", "/add-feedback", "/all-feedbacks", "/delete-feedback")
                 .hasRole("MEMBER")
-                .requestMatchers("/admin/**","/add-event","/add-notice").hasRole("ADMIN")
+                .requestMatchers("/admin/**","/add-event","/add-notice", "/all-feedbacks").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
