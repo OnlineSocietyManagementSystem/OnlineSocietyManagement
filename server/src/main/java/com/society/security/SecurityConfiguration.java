9b3fe240c3ff76868274db762c7192bb1a895bae
D4-87101-SrushtiParlikar <parlikarsrushti@gmail.com>
2025-01-30 16:47:08 +0530

    getAll complaints added

--- server/src/main/java/com/society/security/SecurityConfiguration.java
+++ server/src/main/java/com/society/security/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
-                    "/auth/register", "/auth/signin", 
+                    "/auth/register", "/auth/signin","/all-complaints",
                     "/v*/api-doc*/**", "/swagger-ui/**"
                 ).permitAll()
                 .requestMatchers(HttpMethod.OPTIONS).permitAll()
