85a1903d3010fad5318d8a5ae2d6cf9ee9449b88
pavankumar <pavankumar976747@gmail.com>
2025-01-30 15:27:11 +0530

    complaint controller modified

--- server/src/main/java/com/society/controllers/ComplaintController.java
+++ server/src/main/java/com/society/controllers/ComplaintController.java
@@ -1,5 +1,7 @@
 package com.society.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.jwt.Jwt;
 
@@ -7,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +40,9 @@ public class ComplaintController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage()));
         }
     }
+    
+  
+    
 }
 
 
