21b88617cbd66cbd1d49c7b548386bbd8066c9b5
Dhananjay Gayke <dhanu915818@gmail.com>
2025-02-03 12:10:09 +0530

    Added new functionalities in Resources

--- server/src/main/java/com/society/controllers/ResourceController.java
+++ server/src/main/java/com/society/controllers/ResourceController.java
@@ -1,23 +1,31 @@
 package com.society.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.society.dtos.ApiResponse;
-import com.society.dtos.ResourceDto;
+import com.society.dtos.ResourceRequestDto;
+import com.society.dtos.ResourceResponseDto;
 import com.society.service.ResourceService;
 
+@RestController
 public class ResourceController {
 
 	@Autowired
 	private ResourceService resourceService;
 	
 	@PostMapping("/add-resource")
-	public ResponseEntity<?> addResource(@RequestBody ResourceDto dto, Authentication authentication) {
+	public ResponseEntity<?> addResource(@RequestBody ResourceRequestDto dto, Authentication authentication) {
 		try {
 			String email = authentication.getName();
 			System.out.println("Extracted User Email : " + email);
@@ -26,6 +34,32 @@ public class ResourceController {
 			return ResponseEntity.status(HttpStatus.CREATED).body(response);
 			
 		}catch (RuntimeException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage()));
+		}
+	}
+	
+	@GetMapping("/all-resources")
+	public ResponseEntity<?> getAllResources() {
+		try {
+			List<ResourceResponseDto> resources = resourceService.getAllResources();
+			
+			if(resources.isEmpty()) {
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			} else {
+				return ResponseEntity.ok(resources);
+			}
+		} catch (RuntimeException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage()));
+		}
+	}
+	
+	@DeleteMapping("/delete-resource/{resourceId}")
+	public ResponseEntity<?> deleteResource(@PathVariable Long resourceId, Authentication authentication) {
+		try {
+			String email = authentication.getName();
+			ApiResponse response = resourceService.deleteResource(resourceId, email);
+			return ResponseEntity.ok(response);
+		} catch (RuntimeException e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage()));
 		}
 	}
--- server/src/main/java/com/society/dtos/ResourceResponseDto.java
+++ server/src/main/java/com/society/dtos/ResourceResponseDto.java
@@ -2,14 +2,12 @@ package com.society.dtos;
 
 import java.time.LocalTime;
 
-import com.society.pojos.Society;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ResourceDto extends BaseDto {
+public class ResourceResponseDto extends BaseDto {
 
     private String resourceType;
 
@@ -20,12 +18,10 @@ public class ResourceDto extends BaseDto {
     private boolean availability;
     
     private int capacity;
-       
+    
     private double bookingFee;
-
+    
     private LocalTime openHours;
-
+    
     private String amenities;
-
-    private Society society;
 }
