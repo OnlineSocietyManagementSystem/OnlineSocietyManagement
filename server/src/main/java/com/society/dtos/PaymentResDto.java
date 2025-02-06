171a763238adf3a85723a49ddb3cd0c2e17a30db
Dhananjay Gayke <dhanu915818@gmail.com>
2025-02-06 12:32:52 +0530

    Payment implmentation

--- server/src/main/java/com/society/dtos/PaymentResDto.java
+++ server/src/main/java/com/society/dtos/PaymentResDto.java
@@ -11,7 +11,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class PaymentResDto {
+public class PaymentResDto extends BaseDto{
 
 	    private BigDecimal amount;
 	    private String paymentType;
