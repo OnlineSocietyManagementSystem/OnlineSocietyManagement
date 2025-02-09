1d7bbd9ac71c60bfa5ef351da0e5c443ca9b9b81
D4-87101-SrushtiParlikar <parlikarsrushti@gmail.com>
2025-02-09 16:27:48 +0530

    soft delete for event & complaints

--- server/src/main/java/com/society/daos/ComplaintDao.java
+++ server/src/main/java/com/society/daos/ComplaintDao.java
@@ -7,10 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.society.pojos.Complaints;
+import com.society.pojos.ActivityStatus;
 
 @Repository
 public interface ComplaintDao extends JpaRepository<Complaints,Long> {
 
 	List<Complaints> findByUserId(Long userId);
+	List<Complaints> findByActivityStatus(ActivityStatus ACTIVE);
+	List<Complaints> findByUserIdAndActivityStatus(Long userId,ActivityStatus ACTIVE);
 
 }
