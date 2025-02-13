2627fbcbbb37bfe1d2f97b27cba9c4272d255b97
pavankumar <pavankumar976747@gmail.com>
2025-02-13 17:37:53 +0530

    apis changed

--- client/src/screens/auth_screens/Register.jsx
+++ client/src/screens/auth_screens/Register.jsx
@@ -41,7 +41,7 @@ const Register = () => {
     }
 
     try {
-      const response = await axios.post("http://13.201.73.36:8080/auth/register", {
+      const response = await axios.post("https://13.201.73.36:8080/auth/register", {
         firstName,
         lastName,
         email,
